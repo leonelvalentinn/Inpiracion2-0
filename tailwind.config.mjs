271e5f245e69d979d01ccf1bf7079beca27e45ae
leonelvalentinn <leonelvalentinn@gmail.com>
2024-07-15 18:44:20 -0600

    New Components

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			screens: {
+				xxs: '375px',
+				xs: '425px',
+				xsm: '465px',
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px',
+				'2xl': '1536px'
+			}
+		},
+	},
+	plugins: [],
+}
